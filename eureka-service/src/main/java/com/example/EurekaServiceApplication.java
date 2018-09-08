a642f09a635f326d3f0aff8f69c111765ad6a226
AleksandarKovachev <akovachev@tu-sofia.bg>
2018-09-08 20:59:48 +0300

    Add Eureka service.

---
+++ eureka-service/src/main/java/com/example/EurekaServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * The starter of the Eureka Server
+ * 
+ * @author aleksandar.kovachev
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+}
