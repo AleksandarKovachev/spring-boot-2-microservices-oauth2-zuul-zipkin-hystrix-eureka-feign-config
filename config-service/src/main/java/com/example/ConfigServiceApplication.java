a07580eaf01532fec20ba921e0901a3add1e12ab
AleksandarKovachev <akovachev@tu-sofia.bg>
2018-09-08 22:12:11 +0300

    Add Config service.

---
+++ config-service/src/main/java/com/example/ConfigServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * The starter of the Config Server
+ * 
+ * @author aleksandar.kovachev
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
